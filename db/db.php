01610510faeaed0a6e64d61857d64bcc433c61b9
filipjt88 <filip88ks@gmail.com>
2025-03-02 22:22:15 +0100

    save

--- db/db.php
+++ db/db.php
@@ -8,7 +8,7 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",$username,$password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e) {
-    die('Konekcija nije uspela' . $e->getMessage());
+    die('Konekcija sa bazom nije uspela' . $e->getMessage());
 }
 
 ?>
--- tasks.php
+++ tasks.php
@@ -3,11 +3,23 @@ header("Content-type: application/json");
 require("db/db.php");
 
 if($_SERVER["REQUEST_METHOD"] === "GET") {
+    if(!$pdo) {
+        die(json_encode(["error" => "Konekcija sa bazom nije uspela!"]));
+    }
     $stmt = $pdo->query("SELECT * FROM tasks ORDER BY id DESC");
     $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($tasks);
     exit;
+}
 
+// Add new task
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    if(!isset($data["title"]) || empty(trim($data["title"]))) {
+        echo json_encode(["error" => "Title is requared!"]);
+        exit;
+    }
 
 $title = trim($data["title"]);
 $stmt = $pdo->prepare("INSERT INTO tasks (title) VALUES (:title)");
